a4fcdd3b97298ba581dc89dda8dc364f48c30e58
Richard Harrington <harrington.richard@gmail.com>
2018-10-14 12:51:19 -0400

    more descriptive table names

--- scripts/normalize.sql
+++ scripts/normalize.sql
@@ -1,38 +1,38 @@
--- cd (which ab depends on)
+-- inner_table (which outer_table depends on)
 
-DROP TABLE IF EXISTS cd CASCADE;
+DROP TABLE IF EXISTS inner_table CASCADE;
 
-CREATE TABLE cd(
+CREATE TABLE inner_table(
   id SERIAL PRIMARY KEY,
   external_id INT UNIQUE,
-  c TEXT,
-  d TEXT
+  b TEXT,
+  c TEXT
 );
 
-INSERT INTO cd(external_id, c, d)
+INSERT INTO inner_table(external_id, b, c)
 (
-  SELECT (j.doc::JSON -> 'b' ->> 'external_id')::INT,
-         j.doc::JSON -> 'b' ->> 'c',
-         j.doc::JSON -> 'b' ->> 'd'
+  SELECT (j.doc::JSON -> 'inner_table' ->> 'external_id')::INT,
+         j.doc::JSON -> 'inner_table' ->> 'b',
+         j.doc::JSON -> 'inner_table' ->> 'c'
   FROM import.raw_json AS j
 )
 ON CONFLICT (external_id) DO NOTHING;
 
--- ab (which depends on cd)
+-- outer_table (which depends on inner_table)
 
-DROP TABLE IF EXISTS ab;
+DROP TABLE IF EXISTS outer_table;
 
-CREATE TABLE ab(
+CREATE TABLE outer_table(
   id SERIAL PRIMARY KEY,
   external_id INT UNIQUE,
   a INT,
-  b INT REFERENCES cd (external_id)
+  inner_table_external_id INT REFERENCES inner_table (external_id)
 );
 
-INSERT INTO ab(external_id, a, b)
+INSERT INTO outer_table(external_id, a, inner_table_external_id)
 (
   SELECT (j.doc::JSON ->> 'external_id')::INT,
          (j.doc::JSON ->> 'a')::INT,
-         (j.doc::JSON -> 'b' ->> 'external_id')::INT
+         (j.doc::JSON -> 'inner_table' ->> 'external_id')::INT
   FROM import.raw_json AS j
 );
